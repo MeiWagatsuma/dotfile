0fd8ecd203bea46ea9f3a0ea2289dc42e4feb157
MeiWagatsuma <wagamei256@gmail.com>
2022-06-22 10:52:32 +0900

    [clean] Removed the Path from .zsh/config.zsh and move to .zshenv

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -1,11 +1,2 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
-
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-
 export FZF_TMUX_OPTS="-p 80%"
 
